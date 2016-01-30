4f947613c2e771ae96b59dd10680b3f963e46ece
Jenna Espezua & Kristie Chow <pair+espezua.kristiechow@devbootcamp.com>
2016-01-30 15:04:29 -0500

    Some working survey taking

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,4 +2,8 @@ class Survey < ActiveRecord::Base
   has_many :questions
 
   belongs_to :creator, class_name: 'User', foreign_key: 'creator_id'
+
+  def first_question_id(id)
+    Question.where(survey_id:id).minimum(:id)
+  end
 end
